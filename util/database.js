e806dc210135ecab90cc6f9b0ad67f5565420b4e
Quankai0808 <23042734@myrp.edu.sg>
2024-08-02 10:38:36 +0800

    mysql

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('c237_carport_project', 'root', '', {
 dialect: 'mysql',
-host: 'ysql-kai.alwaysdata.net',
+host: 'mysql-kai.alwaysdata.net',
 user: 'kai_0808',
 password: '657GunZ879',
 database: 'kai_0808'
